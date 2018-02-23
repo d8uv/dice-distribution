d66bb452798b28fd10c3f8990e95f6c2fc3fbf55
d8uv <d8uv@d8uv.org>
2018-02-22 22:30:58 -0600

    add summary, better styling, and different distributions

--- src/App.js
+++ src/App.js
@@ -1,97 +1,8 @@
 import React, { PureComponent } from "react";
+import Calculator from "./Calculator";
 import "./App.css";
 
 const getDiceLevel = (mean, stddev, level) => Math.floor(mean + stddev * level);
-
-class NumberInput extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" + this.props.value };
-  }
-  componentWillReceiveProps(nextprops) {
-    this.setState({ value: "" + nextprops.value });
-  }
-  handleChange = event => {
-    const value = event.target.value;
-    this.setState({ value: value });
-    if (value !== "") {
-      this.props.handleChange(event);
-    }
-  };
-  handleBlur = event => {
-    const value = event.target.value;
-    if (value === "") {
-      this.handleChange({ target: { value: this.props.min } });
-    } else {
-      this.handleChange({ target: { value: Math.round(value) } });
-    }
-  };
-  render() {
-    return (
-      <input
-        type="number"
-        value={this.state.value}
-        onChange={this.handleChange}
-        onBlur={this.handleBlur}
-        step="1"
-        min={this.props.min}
-      />
-    );
-  }
-}
-
-const InputRow = props => (
-  <p>
-    <label>
-      {props.label}: {props.children}
-    </label>
-  </p>
-);
-
-const LevelRow = props => (
-  <li className={props.className}>
-    <span className="col lhs">{props.lhs}</span>{" "}
-    <span className="col eq">=</span>{" "}
-    <span className="col rhs">{props.rhs}</span>
-  </li>
-);
-
-const Calculator = props => (
-  <div id="calculator">
-  <form>
-    <InputRow label="A">
-      <NumberInput value={props.A} handleChange={props.changeA} min={1} />
-    </InputRow>
-    <InputRow label="X">
-      <select value={props.X} onChange={props.changeX}>
-        {[2, 3, 4, 6, 8, 10, 12, 20, 100].map(X => (
-          <option key={X} value={X}>
-            d{X}
-          </option>
-        ))}
-      </select>
-    </InputRow>
-    <InputRow label="B">
-      <NumberInput value={props.B} handleChange={props.changeB} min={0} />
-    </InputRow>
-    </form>
-    <hr />
-    <p className="formula">
-      {props.A}d{props.X} + {props.B}
-    </p>
-    <ul className="levels">
-      <LevelRow lhs="⌊μ−2σ⌋" rhs={props.levels[0]} className="sigma-2" />
-      <LevelRow lhs="⌊μ−σ⌋" rhs={props.levels[1]} className="sigma-1" />
-      <LevelRow lhs="⌊μ⌋" rhs={props.levels[2]} className="sigma-0" />
-      <LevelRow lhs="⌊μ+σ⌋" rhs={props.levels[3]} className="sigma-1" />
-      <LevelRow lhs="⌊μ+2σ⌋" rhs={props.levels[4]} className="sigma-2" />
-    </ul>
-    <ul className="nerd-stats">
-      <li>μ = {props.mean}</li>
-      <li>σ ≈ {props.stddev.toFixed(6)}</li>
-    </ul>
-  </div>
-);
 
 class App extends PureComponent {
   constructor(props) {
@@ -103,10 +14,14 @@ class App extends PureComponent {
   changeB = event => this.setState({ B: parseInt(event.target.value, 10) });
 
   render() {
-    const mean = this.state.A * (1 + this.state.X) / 2 + this.state.B;
-    const stddev =
-      Math.sqrt(this.state.A * (Math.pow(this.state.X, 2) - 1)) /
-      (2 * Math.sqrt(3));
+    const A = this.state.A;
+    const X = this.state.X;
+    const B = this.state.B;
+    const mean = A * (1 + X) / 2 + B;
+    var stddev = Math.sqrt(A * (Math.pow(X, 2) - 1)) / (2 * Math.sqrt(3));
+    if (A === 2) {
+      stddev = (2 * X - 2) / (2 * Math.sqrt(6));
+    }
     const levels = [-2, -1, 0, 1, 2].map(level =>
       getDiceLevel(mean, stddev, level)
     );
@@ -115,9 +30,9 @@ class App extends PureComponent {
       <div className="App">
         <h1>Dice to Distribution</h1>
         <Calculator
-          A={this.state.A}
-          X={this.state.X}
-          B={this.state.B}
+          A={A}
+          X={X}
+          B={B}
           changeA={this.changeA}
           changeX={this.changeX}
           changeB={this.changeB}
---
+++ src/Calculator.js
@@ -0,0 +1,186 @@
+import React, { PureComponent, Fragment } from "react";
+
+class NumberInput extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" + this.props.value };
+  }
+  componentWillReceiveProps(nextprops) {
+    this.setState({ value: "" + nextprops.value });
+  }
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({ value: value });
+    if (value !== "") {
+      this.props.handleChange(event);
+    }
+  };
+  handleBlur = event => {
+    const value = event.target.value;
+    if (value === "") {
+      this.handleChange({ target: { value: this.props.min } });
+    } else {
+      this.handleChange({ target: { value: Math.round(value) } });
+    }
+  };
+  render() {
+    return (
+      <input
+        type="number"
+        value={this.state.value}
+        onChange={this.handleChange}
+        onBlur={this.handleBlur}
+        step="1"
+        min={this.props.min}
+      />
+    );
+  }
+}
+
+const InputRow = props => (
+  <p>
+    <label>
+      {props.label}: {props.children}
+    </label>
+  </p>
+);
+
+const LevelRow = props => (
+  <li className={props.className}>
+    <span className="col lhs">{props.lhs}</span>{" "}
+    <span className="col eq">=</span>{" "}
+    <span className="col rhs">{props.rhs}</span>
+  </li>
+);
+
+const Calculator = props => {
+  var levels;
+  switch (props.A) {
+    case 1:
+      levels = (
+        <Fragment>
+          <ul className="levels">
+            <LevelRow
+              lhs="min"
+              rhs={1 + props.B}
+              className="sigma-0 italic-lhs"
+            />
+            <LevelRow
+              lhs="max"
+              rhs={props.X + props.B}
+              className="sigma-0 italic-lhs"
+            />
+          </ul>
+          <ul className="nerd-stats">
+            <li>μ = {props.mean}</li>
+          </ul>
+        </Fragment>
+      );
+      break;
+    case 2:
+      levels = (
+        <Fragment>
+          <ul className="levels">
+            <LevelRow
+              lhs="min"
+              rhs={2 + props.B}
+              className="sigma-2 italic-lhs"
+            />
+            <LevelRow lhs="⌊μ−σ⌋" rhs={props.levels[1]} className="sigma-1" />
+            <LevelRow lhs="⌊μ⌋" rhs={props.levels[2]} className="sigma-0" />
+            <LevelRow lhs="⌊μ+σ⌋" rhs={props.levels[3]} className="sigma-1" />
+            <LevelRow
+              lhs="max"
+              rhs={props.X * 2 + props.B}
+              className="sigma-2 italic-lhs"
+            />
+          </ul>
+          <ul className="nerd-stats">
+            <li>μ = {props.mean}</li>
+            <li>σ ≈ {props.stddev.toFixed(6)}</li>
+          </ul>
+        </Fragment>
+      );
+      break;
+    default:
+      levels = (
+        <Fragment>
+          <ul className="levels">
+            <LevelRow lhs="⌊μ−2σ⌋" rhs={props.levels[0]} className="sigma-2" />
+            <LevelRow lhs="⌊μ−σ⌋" rhs={props.levels[1]} className="sigma-1" />
+            <LevelRow lhs="⌊μ⌋" rhs={props.levels[2]} className="sigma-0" />
+            <LevelRow lhs="⌊μ+σ⌋" rhs={props.levels[3]} className="sigma-1" />
+            <LevelRow lhs="⌊μ+2σ⌋" rhs={props.levels[4]} className="sigma-2" />
+          </ul>
+          <ul className="nerd-stats">
+            <li>μ = {props.mean}</li>
+            <li>σ ≈ {props.stddev.toFixed(6)}</li>
+          </ul>
+        </Fragment>
+      );
+  }
+  var summary;
+  switch (props.A) {
+    case 1:
+      break;
+    case 2:
+      summary = (
+        <p className="calculator-summary">
+          When you roll {props.A} {props.X}-sided die, the result will{" "}
+          <span className="tooltip" title="50%">
+            likely
+          </span>{" "}
+          be between {props.levels[1]} and {props.levels[3]} (usually around{" "}
+          {props.levels[2]}).
+        </p>
+      );
+      break;
+    default:
+      summary = (
+        <p className="calculator-summary">
+          When you roll {props.A} {props.X}-sided die, the result will{" "}
+          <span className="tooltip" title="68%">
+            likely
+          </span>{" "}
+          be between {props.levels[1]} and {props.levels[3]} (usually around{" "}
+          {props.levels[2]}). The result will{" "}
+          <span className="tooltip" title="95%">
+            rarely
+          </span>{" "}
+          be below {props.levels[0]}, or above {props.levels[4]}.
+        </p>
+      );
+  }
+
+  return (
+    <Fragment>
+      <div id="calculator">
+        <form>
+          <InputRow label="A">
+            <NumberInput value={props.A} handleChange={props.changeA} min={1} />
+          </InputRow>
+          <InputRow label="X">
+            <select value={props.X} onChange={props.changeX}>
+              {[2, 3, 4, 6, 8, 10, 12, 20, 100].map(X => (
+                <option key={X} value={X}>
+                  d{X}
+                </option>
+              ))}
+            </select>
+          </InputRow>
+          <InputRow label="B">
+            <NumberInput value={props.B} handleChange={props.changeB} min={0} />
+          </InputRow>
+        </form>
+        <hr />
+        <p className="formula">
+          {props.A}d{props.X} + {props.B}
+        </p>
+        {levels}
+      </div>
+      {summary}
+    </Fragment>
+  );
+};
+
+export default Calculator;
