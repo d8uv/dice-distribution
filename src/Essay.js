7b051491645650b6b2cedd64ab06256ddff43d06
d8uv <d8uv@d8uv.org>
2018-02-25 22:36:06 -0600

    finding normal distribution w/ mean sections

--- src/Essay.js
+++ src/Essay.js
@@ -20,20 +20,54 @@ const Essay = props => (
       <a href="https://en.wikipedia.org/wiki/Dice_notation">dice notation</a>,
       it’s pretty simple. It follows the format{" "}
       <mark>
-        <i>A</i>d<i>X</i> + <i>B</i>
-      </mark>, where <i>A</i> is the number of dice being rolled, <i>X</i> is
-      the number of sides on each die, and <i>B</i> is a number you add to the
-      result. So, if you’re rolling {toWords(props.A)}{" "}
+        <var>A</var>d<var>X</var> + <var>B</var>
+      </mark>, where <var>A</var> is the number of dice being rolled,{" "}
+      <var>X</var> is the number of sides on each die, and <var>B</var> is a
+      number you add to the result. So, if you’re rolling {toWords(props.A)}{" "}
       {toWords(props.X).replace(" ", "-")}-sided die and{" "}
-      {props.B < 0 ? "subtracting" : "adding"}{" "}
-      {toWords(props.B)}, that makes{" "}
+      {props.B < 0 ? "subtracting" : "adding"} {toWords(props.B)}, that makes{" "}
       <mark>
-        <i>A</i> = {props.A}, <i>X</i> = {props.X}, and <i>B</i> = {props.B}
+        <var>A</var> = {props.A}, <var>X</var> = {props.X}, and <var>B</var> ={" "}
+        {props.B}
       </mark>, or{" "}
       <mark>
         {props.A}d{props.X} {props.B < 0 ? "−" : "+"} {Math.abs(props.B)}
       </mark>.
     </p>
+    <h3>Finding the Normal Distribution</h3>
+    <p>
+      In order to find the normal distribution, we need to find two things: The{" "}
+      <em>
+        <a href="https://en.wikipedia.org/wiki/Mean">mean</a> (μ)
+      </em>, and the{" "}
+      <em>
+        <a href="https://en.wikipedia.org/wiki/Standard_deviation">
+          standard deviation
+        </a>{" "}
+        (σ)
+      </em>.
+    </p>
+    <p>
+      The mean is the most common result. It’s the number in the middle, and the
+      most likely to show up, due to it having the most number of possible ways
+      to roll it. Here’s how to find the mean:
+    </p>
+    <div className="formula">
+      <p>
+        <span className="expression">mean</span>{" "}
+        <span className="expression">
+          =
+          <mark>
+            μ(<var>A</var>, <var>X</var>, <var>B</var>) = (<var>A</var> × (1 +{" "}
+            <var>X</var>))/2 + <var>B</var>
+          </mark>
+        </span>{" "}
+        <span className="expression">
+          = ({props.A} × (1 + {props.X}))/2 + {props.B}
+        </span>{" "}
+        <span className="expression">= {props.mean}</span>
+      </p>
+    </div>
   </div>
 );
 
