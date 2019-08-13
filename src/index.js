d1891f74cf9bd83c16f1cccad4ac930bf522b2bb
d8uv <d8uv@d8uv.org>
2019-08-12 21:04:02 -0500

    use react-snap instead of react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { render } from 'react-snapshot';
+import { hydrate, render } from "react-dom";
 import "./index.css";
 import App from "./App";
 // import registerServiceWorker from "./registerServiceWorker";
 
-render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 // registerServiceWorker();
